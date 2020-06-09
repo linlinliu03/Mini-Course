e633dfe004c7eb5bf5ec0daec8045ac934799cf4
linlinliu <linlinliu03@gmail.com>
2020-06-09 13:39:16 -0700

    Setup frontend folder

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import "bootstrap/dist/css/bootstrap.css";
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
