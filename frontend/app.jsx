d43c46f71ad6cb0d096d0755ec4ca1b4ccaa84b7
linlinliu <linlinliu03@gmail.com>
2020-06-09 22:19:42 -0700

    Add Home component

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home/Home";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to Mini Course</h1>, root);
+    ReactDOM.render(<Home />, root);
 });
---
+++ frontend/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+class Home extends Component{
+   constructor(){
+       super()
+       this.state = {
+           video_modules : [
+
+           ]
+       }
+   }
+
+
+   render(){
+       return(
+           <div>This is Home Component!</div>
+       )
+   }
+}
+
+export default Home;
