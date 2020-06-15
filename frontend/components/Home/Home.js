a614a2c9dc0d47b6144154b4b3752d9c36de16c4
linlinliu <linlinliu03@gmail.com>
2020-06-15 08:55:08 -0700

    Add some example data for Home component

--- frontend/components/Home/Home.js
+++ frontend/components/Home/Home.js
@@ -5,7 +5,10 @@ class Home extends Component{
        super()
        this.state = {
            video_modules : [
-
+                 {id:1, title:"1. Setting up a new Ruby on Rails App with React.", description: "lorem ipsum", active: false},
+               { id: 2, title: "2. Adding React to an Existing Rails App.", description: "lorem ipsum", active: false},
+               { id: 3, title: "3. Building a Hello World App.", description: "lorem ipsum", active: false},
+               { id: 4, title: "4. Adding REact Router Dom to your App.", description: "lorem ipsum", active: false}
            ]
        }
    }
