1d0680a4f65bb4ede4ccd924ec1413043bc321ae
linlinliu <linlinliu03@gmail.com>
2020-06-17 21:05:47 -0700

    Add more styles

--- frontend/components/Home/Jumbotron.js
+++ frontend/components/Home/Jumbotron.js
@@ -8,6 +8,18 @@ const Section = styled.section`
     color: #fff;
 `
 
+const Header = styled.h1`
+    color: #fff;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 52px;
+` 
+
+const Subhead = styled.p`
+    font-size: 18px;
+    font-weight: 500;
+` 
+
 const Jumbotron = () => {
     return(
        <Section>
@@ -15,8 +27,8 @@ const Jumbotron = () => {
                <div className="row">
                   <div className="col col-sm-12 col-md-5">
                       <div className="pt-4 mt-4">
-                          <h1>React For Rails Developers!</h1>
-                          <p>Supercharge your Ruby on Rails App with React.js</p>
+                          <Header>React For Rails Developers!</Header>
+                          <Subhead>Supercharge your Ruby on Rails App with React.js</Subhead>
                       </div>
                   </div>
                   <div className="col col-sm-12 col-md-7">
