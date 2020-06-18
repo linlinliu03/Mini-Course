3321b8c99bc768f9b215ab1b314168e59ca52d1f
linlinliu <linlinliu03@gmail.com>
2020-06-17 21:17:53 -0700

    Add styled button

--- frontend/components/Home/Jumbotron.js
+++ frontend/components/Home/Jumbotron.js
@@ -20,6 +20,21 @@ const Subhead = styled.p`
     font-weight: 500;
 ` 
 
+const Button = styled.a`
+    display: inline-block;
+    text-decoration: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0;
+    background: #fff;
+    color: #333 !important;
+    padding: 10px 20px;
+    font-size: 18px;
+    box-shadow: 0px 0px 0px 3px #473228,
+       -6px 6px #ef5f17,
+       -6px 6px 0px 3px #473228;
+` 
+
 const Jumbotron = () => {
     return(
        <Section>
@@ -29,6 +44,9 @@ const Jumbotron = () => {
                       <div className="pt-4 mt-4">
                           <Header>React For Rails Developers!</Header>
                           <Subhead>Supercharge your Ruby on Rails App with React.js</Subhead>
+                          <div className="cta-wrapper">
+                              <Button className="btn fancy-btn">Get Started</Button>
+                          </div>
                       </div>
                   </div>
                   <div className="col col-sm-12 col-md-7">
